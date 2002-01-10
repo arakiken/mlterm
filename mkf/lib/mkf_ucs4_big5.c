425e15b12e21db29e3b33c37eab656ea6e0c0dc0
arakiken <devnull@localhost>
2002-01-10 06:21:01 +0000

    * big5 unstandard characters can be converted to and from ucs4.

--- mkf/lib/mkf_ucs4_big5.c
+++ mkf/lib/mkf_ucs4_big5.c
@@ -4,29 +4,18 @@
 
 #include  "mkf_ucs4_big5.h"
 
-/*
- * these conversion tables are for BIG5 + HKSCS.
- */
-
 #include  "table/mkf_big5_to_ucs4.table"
+#include  "table/mkf_hkscs_to_ucs4.table"
 
 #include  "table/mkf_ucs4_alphabet_to_big5.table"
 #include  "table/mkf_ucs4_cjk_to_big5.table"
 #include  "table/mkf_ucs4_compat_to_big5.table"
 #include  "table/mkf_ucs4_pua_to_big5.table"
-
-
-/*
- * the same macro is defined in mkf_big5_parser.c
- */
-#define  IS_HKSCS(code) \
-	( (0x8140 <= (code) && (code) <= 0xa0fe) || \
-	  (0xc6a1 <= (code) && (code) <= 0xc8fe) || \
-	  (0xf9d6 <= (code) && (code) <= 0xfefe) )
-
-#define  IS_HKSCS_ONLY(code) \
-	( (0x8140 <= (code) && (code) <= 0xa0fe) || \
-	  (0xf9d6 <= (code) && (code) <= 0xfefe) )
+#include  "table/mkf_ucs4_alphabet_to_hkscs.table"
+#include  "table/mkf_ucs4_cjk_to_hkscs.table"
+#include  "table/mkf_ucs4_compat_to_hkscs.table"
+#include  "table/mkf_ucs4_extension_a_to_hkscs.table"
+#include  "table/mkf_ucs4_pua_to_hkscs.table"
 
 
 /* --- global functions --- */
@@ -38,11 +27,6 @@ mkf_map_big5_to_ucs4(
 	)
 {
 	u_int32_t  c ;
-
-	if( IS_HKSCS_ONLY(big5))
-	{
-		return  0 ;
-	}
 	
 	if( ( c = CONV_BIG5_TO_UCS4(big5)))
 	{
@@ -65,7 +49,7 @@ mkf_map_hkscs_to_ucs4(
 {
 	u_int32_t  c ;
 
-	if( ( c = CONV_BIG5_TO_UCS4(hkscs)))
+	if( ( c = CONV_HKSCS_TO_UCS4(hkscs)))
 	{
 		mkf_int_to_bytes( ucs4->ch , 4 , c) ;
 		ucs4->size = 4 ;
@@ -91,11 +75,6 @@ mkf_map_ucs4_to_big5(
 		( c = CONV_UCS4_COMPAT_TO_BIG5(ucs4_code)) ||
 		( c = CONV_UCS4_PUA_TO_BIG5(ucs4_code)))
 	{
-		if( IS_HKSCS_ONLY(c))
-		{
-			return  0 ;
-		}
-
 		mkf_int_to_bytes( big5->ch , 2 , c) ;
 		big5->size = 2 ;
 		big5->cs = BIG5 ;
@@ -115,16 +94,12 @@ mkf_map_ucs4_to_hkscs(
 {
 	u_int16_t  c ;
 
-	if( ( c = CONV_UCS4_ALPHABET_TO_BIG5(ucs4_code)) ||
-		( c = CONV_UCS4_CJK_TO_BIG5(ucs4_code)) ||
-		( c = CONV_UCS4_COMPAT_TO_BIG5(ucs4_code)) ||
-		( c = CONV_UCS4_PUA_TO_BIG5(ucs4_code)))
+	if( ( c = CONV_UCS4_ALPHABET_TO_HKSCS(ucs4_code)) ||
+		( c = CONV_UCS4_CJK_TO_HKSCS(ucs4_code)) ||
+		( c = CONV_UCS4_COMPAT_TO_HKSCS(ucs4_code)) ||
+		( c = CONV_UCS4_EXTENSION_A_TO_HKSCS(ucs4_code)) ||
+		( c = CONV_UCS4_PUA_TO_HKSCS(ucs4_code)))
 	{
-		if( ! IS_HKSCS(c))
-		{
-			return  0 ;
-		}
-
 		mkf_int_to_bytes( hkscs->ch , 2 , c) ;
 		hkscs->size = 2 ;
 		hkscs->cs = HKSCS ;
