715a1af1c91cea51982d931983bd820faf55de26
seiichi <devnull@localhost>
2002-04-30 18:22:24 +0000

    added a comment for exsb_get_pixel()

--- contrib/scrollbar/extra/exsb_common.h
+++ contrib/scrollbar/extra/exsb_common.h
@@ -1,10 +1,11 @@
 /*
- *	$Id $
+ *	$Id$
  */
 
 #ifndef  __EXSB_COMMON_H__
 #define  __EXSB_COMMON_H__
 
+/* If color_name is not found in default color map, returns nearest pixel */
 unsigned long exsb_get_pixel( Display * display , int screen , char * color_name) ;
 
 #endif
