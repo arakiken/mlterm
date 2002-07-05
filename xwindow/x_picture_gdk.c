2f628a1af8966e895e15441c208fb7d4ea8b6ae9
h_minami <devnull@localhost>
2002-07-05 08:34:04 +0000

    minor code cleanup

--- xwindow/x_picture_gdk.c
+++ xwindow/x_picture_gdk.c
@@ -17,10 +17,24 @@
 
 /* --- static variables --- */
 
-/* gdk-pixbuf does not need a black magic using static variables */
-
+static int display_count = 0;
+static unsigned char gamma_cache[255];
 /* --- static functions --- */
 
+static unsigned char
+modify_gamma(unsigned char value, x_picture_modifier_t *  pic_mod){
+	if (value == 255)
+		return 255;
+	if (value == 0)
+		return 0;
+
+	if (gamma_cache[value]){
+
+		return gamma_cache[value];
+	}
+	else 
+		return gamma_cache[value] = 255 * pow((double)value / 255, (double)pic_mod->gamma / 100);
+}
 
 static unsigned char
 modify_color(unsigned char value, x_picture_modifier_t *  pic_mod){
@@ -31,12 +45,9 @@ modify_color(unsigned char value, x_picture_modifier_t *  pic_mod){
 		return 255;
  	if (result <= 0)
 		return 0;
-/* XXX  LUT should be made*/
-/* default gamma may be machine dependent?*/
-	if (pic_mod->gamma != 100)
-		result = 255 * pow((double)result / 255, (double)pic_mod->gamma / 100);
 	return (unsigned char)(result);
 }
+
 static int
 modify_image(
 	GdkPixbuf *  pixbuf ,
@@ -60,7 +71,12 @@ modify_image(
 	rowstride = gdk_pixbuf_get_rowstride (pixbuf);
 	
 	line = gdk_pixbuf_get_pixels (pixbuf);
-		
+	if ((pic_mod->gamma != 100) &&
+	    (pic_mod->gamma != gamma_cache[0])
+		){
+		memset(gamma_cache, 0, sizeof(gamma_cache));
+		gamma_cache[0] = pic_mod->gamma %256 ; /* corrision will never happen actually */
+	}
 	for (i = 0 ; i < height ; i++) {
 		pixel = line;
 		line += rowstride;
@@ -70,6 +86,12 @@ modify_image(
 			pixel[0] = modify_color(pixel[0], pic_mod); 
 			pixel[1] = modify_color(pixel[1], pic_mod);
 			pixel[2] = modify_color(pixel[2], pic_mod);
+			if (pic_mod->gamma != 100)
+			{
+				pixel[0] = modify_gamma(pixel[0], pic_mod); 
+				pixel[1] = modify_gamma(pixel[1], pic_mod);
+				pixel[2] = modify_gamma(pixel[2], pic_mod);
+			}
 			/* alpha is not changed */
 			pixel += bytes_per_pixel;
 		}
@@ -84,11 +106,14 @@ x_picdep_display_opened(
 	Display *  display
 	)
 {
-/* XXX it should be harmless to call them many many times*/
+	if (display_count == 0){
 #ifndef OLD_GDK_PIXBUF
-	g_type_init();
+		g_type_init();
 #endif
-	gdk_pixbuf_xlib_init( display, 0 ); 
+		gdk_pixbuf_xlib_init( display, 0 ); 
+
+	}
+	display_count ++;
 	return  1 ;
 }
 
@@ -97,7 +122,8 @@ x_picdep_display_closed(
 	Display *  display
 	)
 {
-	return  1 ; /* XXX who frees cmap? */
+	display_count --;
+	return  1 ; 
 }
 
 Pixmap
@@ -145,8 +171,6 @@ x_picdep_load_file( x_window_t *  win ,
 	gdk_pixbuf_unref( img );
 	return  pixmap ;
 }
-
-#ifdef  PSEUDO_TRANSPARENT
 
 int
 x_picdep_root_pixmap_available(
@@ -294,140 +318,3 @@ found:
 
 	return  pixmap ;
 }
-
-#else /* ! PSEUDO_TRANSPARENT */
-
-int
-x_picdep_root_pixmap_available(
-	Display *  display
-	)
-{
-	return  1 ;
-}
-
-Pixmap
-x_picdep_load_background(
-	x_window_t *  win ,
-	x_picture_modifier_t *  pic_mod
-	)
-{
-	int  x ;
-	int  y ;
-	int  pix_x ;
-	int  pix_y ;
-	u_int  width ;
-	u_int  height ;
-	Pixmap  pixmap ;
-	GdkPixbuf *  img ;
-	x_window_t *  root ;
-	int  count ;
-	XEvent *  queued_events ;
-	u_int  num_of_queued ;
-	XEvent  event ;
-
-
-	root = x_get_root_window(win) ;
-	
-	if( ! x_window_get_visible_geometry( win , &x , &y , &pix_x , &pix_y , &width , &height))
-	{
-		return  None ;
-	}
-
-	/* XXX already queued StructureNotifyMask events are backuped */
-	queued_events = NULL ;
-	num_of_queued = 0 ;
-	while( XCheckWindowEvent( root->display , root->my_window , StructureNotifyMask , &event))
-	{
-		void *  p ;
-		
-		if( ( p = realloc( queued_events , sizeof( XEvent) * ( num_of_queued + 1))) == NULL)
-		{
-			break ;
-		}
-
-		queued_events = p ;
-		queued_events[num_of_queued ++] = event ;
-	}
-
-	x_window_remove_event_mask( root , StructureNotifyMask /* | SubstructureNotifyMask */) ;
-
-	/*
-	 * StructureNotifyMask events are ignored from here.
-	 */
-	
-	x_window_unmap( root) ;
-
-	/* XXX waiting for all exposed windows actually redrawn. */
-	XSync( root->display , False) ;
-	kik_usleep( 25000) ;
-	
-	img = gdk_pixbuf_xlib_get_from_drawable( NULL ,
-						 DefaultRootWindow( win->display) ,
-						 NULL, NULL , x , y , 0 , 0 , width , height) ;
-
-	/* XXX ignoring all queued Expose events */
-	while( XCheckWindowEvent( root->display , root->my_window , ExposureMask, &event)) ;
-	
-	x_window_map( root) ;
-
-	/*
-	 * StructureNoitfyMask events are ignored till here.
-	 */
-	
-	x_window_add_event_mask( root , StructureNotifyMask /* | SubstructureNotifyMask */) ;
-
-	/* XXX waiting for all StructureNotifyMask events responded */
-	XSync( root->display , False) ;
-	kik_usleep( 25000) ;
-
-	/* XXX ignoring all queued StructureNotifyMask events */
-	while( XCheckWindowEvent( root->display , root->my_window , StructureNotifyMask , &event)) ;
-
-	/* XXX restoring all backuped StructureNotifyMask events */
-	for( count = 0 ; count < num_of_queued ; count ++)
-	{
-		XPutBackEvent( root->display , &queued_events[count]) ;
-	}
-	free( queued_events) ;
-	
-	/* XXX waiting for root window actually mapped. */
-	count = 0 ;
-	while( ! XCheckWindowEvent( root->display , root->my_window , ExposureMask, &event))
-	{
-		kik_usleep( 50000) ;
-
-		if( ++ count >= 10)
-		{
-			return  None ;
-		}
-	}
-	XPutBackEvent( root->display , &event) ;
-
-	if( ! img)
-	{
-		return  None ;
-	}
-
-	if( pic_mod)
-	{
-		modify_image( img , pic_mod) ;
-	}
-
-	pixmap = XCreatePixmap( win->display , win->my_window , ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
-			DefaultDepth( win->display , win->screen)) ;
-	gdk_pixbuf_xlib_render_to_drawable( img , 
-					    pixmap,
-					    gc , 
-					    0 , 0 , pix_x , pix_y ,
-					    ACTUAL_WIDTH(win) ,
-					    ACTUAL_HEIGHT(win) ,
-					    XLIB_RGB_DITHER_NONE,
-					    0 , 0) ;
-
-	gdk_pixbuf_unref( img) ;
-
-	return  pixmap ;
-}
-
-#endif /* PSEUDO_TRANSPARENT */	
-
