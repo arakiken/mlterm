8d5af8ac1ac5f4828f6f6f106e127def00aebca9
h_minami <devnull@localhost>
2002-09-16 17:32:17 +0000

    renamed picdep* to imagelib*.

--- xwindow/x_imagelib_gdk.c
+++ xwindow/x_imagelib_gdk.c
@@ -61,29 +61,32 @@ modify_color(unsigned char value, x_picture_modifier_t *  pic_mod){
 }
 
 static int
-modify_image( GdkPixbuf *  pixbuf, x_picture_modifier_t *  pic_mod){
+modify_image( GdkPixbuf *  pixbuf, GdkPixbuf * root, x_picture_modifier_t *  pic_mod){
 	int i, j;
 	int width, height, rowstride, bytes_per_pixel;
 	unsigned char *line;
 	unsigned char *pixel;
 
-	if( pic_mod->brightness == 100 && pic_mod->contrast == 100 && pic_mod->gamma == 100)
-		return 0;
+	if ( !pixbuf )
+			return 0;
 
-        if ( !pixbuf )
-		return 0;
-  
-	bytes_per_pixel = gdk_pixbuf_get_has_alpha (pixbuf) ? 4 : 3; /* alpha may supported someday  */
+	bytes_per_pixel = (gdk_pixbuf_get_has_alpha( pixbuf)) ? 4:3 ;
 	width = gdk_pixbuf_get_width (pixbuf);
 	height = gdk_pixbuf_get_height (pixbuf);
 	rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+
+	if( !pic_mod)
+		goto marge;
+
+	if(pic_mod->brightness == 100 && pic_mod->contrast == 100 && pic_mod->gamma == 100)
+		return 0;
 	
-	line = gdk_pixbuf_get_pixels (pixbuf);
+       	line = gdk_pixbuf_get_pixels (pixbuf);
 	if ((pic_mod->gamma != 100) &&
-	    (pic_mod->gamma != gamma_cache[0])){
+		    (pic_mod->gamma != gamma_cache[0])){
 		memset(gamma_cache, 0, sizeof(gamma_cache));
 		gamma_cache[0] = pic_mod->gamma %256 ; /* corrision will never happen actually */
-	}
+		}
 	for (i = 0 ; i < height ; i++) {
 		pixel = line;
 		line += rowstride;
@@ -93,98 +96,53 @@ modify_image( GdkPixbuf *  pixbuf, x_picture_modifier_t *  pic_mod){
 			pixel[0] = modify_color(pixel[0], pic_mod); 
 			pixel[1] = modify_color(pixel[1], pic_mod);
 			pixel[2] = modify_color(pixel[2], pic_mod);
-			if (pic_mod->gamma != 100){
-				pixel[0] = modify_gamma(pixel[0], pic_mod); 
-				pixel[1] = modify_gamma(pixel[1], pic_mod);
-				pixel[2] = modify_gamma(pixel[2], pic_mod);
-			}
 			/* alpha is not changed */
 			pixel += bytes_per_pixel;
 		}
 	}
+		
+	if (pic_mod->gamma != 100){
+		line = gdk_pixbuf_get_pixels (pixbuf);
+		for (i = 0 ; i < height ; i++) {
+			pixel = line;
+			line += rowstride;
+			for (j = 0 ; j < width ; j++) {
+				pixel[0] = modify_gamma(pixel[0], pic_mod); 
+				pixel[1] = modify_gamma(pixel[1], pic_mod);
+				pixel[2] = modify_gamma(pixel[2], pic_mod);
+				pixel += bytes_per_pixel;
+			}
+		}
+	}
+ marge:
+	if (root){
+		int r_width, r_height, r_rowstride;
+		unsigned char *r_line;
+		unsigned char *r_pixel;
+		r_line = gdk_pixbuf_get_pixels( root) ;
+		r_rowstride = gdk_pixbuf_get_rowstride (root);
+		line = gdk_pixbuf_get_pixels( pixbuf) ;
+		for (i = 0 ; i < height ; i++) {
+			pixel = line;
+			line += rowstride;
+
+			r_pixel = r_line;
+			r_line += r_rowstride;
+			for (j = 0 ; j < width ; j++) {
+				pixel[0] = ((long)(pixel[0])*pixel[3]+(long)(r_pixel[0])*(255-pixel[3]))/255; 
+				pixel[1] = ((long)(pixel[1])*pixel[3]+(long)(r_pixel[1])*(255-pixel[3]))/255; 
+				pixel[2] = ((long)(pixel[2])*pixel[3]+(long)(r_pixel[2])*(255-pixel[3]))/255; 
+				pixel += 4; /* should have alpha */
+				r_pixel += 3; /* should not have alpha */
+			}
+		}
+		gdk_pixbuf_unref( root );
+	}
 	return 1;
 }
 
-/* --- global functions --- */
-
-int
-x_imagelib_display_opened( Display *  display){
-	if (display_count == 0){
-#ifndef OLD_GDK_PIXBUF
-		g_type_init();
-#endif
-		gdk_pixbuf_xlib_init( display, 0 ); 
-
-	}
-	display_count ++;
-	return  1 ;
-}
-
-int
-x_imagelib_display_closed( Display *  display){
-	display_count --;
-
-	/* XXX
-         *
-         *  there's no way to free mamories alocated from gdk_pixbuf_xlib_init( display, 0 ).
-         *  replacement shou be written
-         */
-	return  1 ; 
-}
-
-Pixmap
-x_imagelib_load_file_for_background( x_window_t *  win , char *  file_path , x_picture_modifier_t *  pic_mod){
-	GdkPixbuf *  img ;
-	GdkPixbuf *  scaled ;
-	Pixmap  pixmap ;
-	GC gc;
-
-#ifndef OLD_GDK_PIXBUF
-	if( ( img = gdk_pixbuf_new_from_file( file_path , NULL )) == NULL)
-		return  None ;
-#else
-	if( ( img = gdk_pixbuf_new_from_file( file_path )) == NULL)
-		return  None ;
-#endif /*OLD_GDK_PIXBUF*/
-
-	if( pic_mod)
-		modify_image( img , pic_mod) ;
-	
-	pixmap = XCreatePixmap( win->display , win->my_window , ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
-			DefaultDepth( win->display , win->screen)) ;
-
-	scaled = gdk_pixbuf_scale_simple(img, ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
-				GDK_INTERP_TILES); /* use one of _NEAREST, _TILES, _BILINEAR, _HYPER (speed<->quality) */
-
-	if (scaled){
-		gdk_pixbuf_unref( img );
-		img = scaled;
-	}
-	gc = XCreateGC( win->display, win->my_window, 0, 0 );
-	gdk_pixbuf_xlib_render_to_drawable( img , 
-					    pixmap,
-					    gc , 
-					    0 , 0 , /* src */
-					    0 , 0 , /* dest */
-					    ACTUAL_WIDTH(win) ,
-					    ACTUAL_HEIGHT(win) ,
-					    XLIB_RGB_DITHER_NORMAL , /* Dither */
-					    0 , 0 /* dither x,y */) ;
-	XFreeGC( win->display , gc );
-	gdk_pixbuf_unref( img );
-	return  pixmap ;
-}
-
-int
-x_imagelib_root_pixmap_available( Display *  display){
-	if( XInternAtom( display , "_XROOTPMAP_ID" , True))
-		return  1 ;
-
-	return  0 ;
-}
-
-Pixmap
-x_imagelib_get_transparent_background( x_window_t *  win , x_picture_modifier_t *  pic_mod){
+GdkPixbuf *
+get_root_pixmap( x_window_t *  win){
 	int  x ;
 	int  y ;
 	int  pix_x ;
@@ -192,12 +150,10 @@ x_imagelib_get_transparent_background( x_window_t *  win , x_picture_modifier_t 
 	u_int  width ;
 	u_int  height ;
 	Window  src ;
-	Pixmap  pixmap ;
 	XSetWindowAttributes attr ;
 	XEvent event ;
 	int count ;
 	GdkPixbuf *  img ;
-	GC gc;
 	Atom id ;
 	
 	if( ! x_window_get_visible_geometry( win , &x , &y , &pix_x , &pix_y , &width , &height))
@@ -276,9 +232,117 @@ x_imagelib_get_transparent_background( x_window_t *  win , x_picture_modifier_t 
 		return  None ;
 
 found:
-	if( pic_mod)
-		modify_image( img , pic_mod) ;
+	return  img ;
+}
+
+/* --- global functions --- */
+
+int
+x_imagelib_display_opened( Display *  display){
+	if (display_count == 0){
+#ifndef OLD_GDK_PIXBUF
+		g_type_init();
+#endif
+		gdk_pixbuf_xlib_init( display, 0 ); 
+
+	}
+	display_count ++;
+	return  1 ;
+}
+
+int
+x_imagelib_display_closed( Display *  display){
+	display_count --;
+
+	/* XXX
+         *
+         *  there's no way to free mamories alocated from gdk_pixbuf_xlib_init( display, 0 ).
+         *  replacement shou be written
+         */
+	return  1 ; 
+}
+
+Pixmap
+x_imagelib_load_file_for_background( x_window_t *  win , char *  file_path , x_picture_modifier_t *  pic_mod){
+	GdkPixbuf *  img ;
+	GdkPixbuf *  scaled ;
+	GdkPixbuf * root;
+	Pixmap  pixmap ;
+	GC gc;
+
+#ifndef OLD_GDK_PIXBUF
+	if( ( img = gdk_pixbuf_new_from_file( file_path , NULL )) == NULL)
+		return  None ;
+#else
+	if( ( img = gdk_pixbuf_new_from_file( file_path )) == NULL)
+		return  None ;
+#endif /*OLD_GDK_PIXBUF*/
+
+	if (gdk_pixbuf_get_has_alpha( img)){
+		root = get_root_pixmap( win);
+	}else{
+		root = NULL;
+	}
+
 	
+	pixmap = XCreatePixmap( win->display , win->my_window , ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
+			DefaultDepth( win->display , win->screen)) ;
+
+	scaled = gdk_pixbuf_scale_simple(img, ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
+				GDK_INTERP_TILES); /* use one of _NEAREST, _TILES, _BILINEAR, _HYPER (speed<->quality) */
+
+	if (scaled){
+		gdk_pixbuf_unref( img );
+		img = scaled;
+	}
+	if( pic_mod || root)
+		modify_image( img , root, pic_mod) ;
+
+	gc = XCreateGC( win->display, win->my_window, 0, 0 );
+	gdk_pixbuf_xlib_render_to_drawable( img , 
+					    pixmap,
+					    gc , 
+					    0 , 0 , /* src */
+					    0 , 0 , /* dest */
+					    ACTUAL_WIDTH(win) ,
+					    ACTUAL_HEIGHT(win) ,
+					    XLIB_RGB_DITHER_NORMAL , /* Dither */
+					    0 , 0 /* dither x,y */) ;
+	XFreeGC( win->display , gc );
+	gdk_pixbuf_unref( img );
+	return  pixmap ;
+}
+
+int
+x_imagelib_root_pixmap_available( Display *  display){
+	if( XInternAtom( display , "_XROOTPMAP_ID" , True))
+		return  1 ;
+
+	return  0 ;
+}
+
+Pixmap
+x_imagelib_get_transparent_background( x_window_t *  win , x_picture_modifier_t *  pic_mod){
+	int  x ;
+	int  y ;
+	int  pix_x ;
+	int  pix_y ;
+	u_int  width ;
+	u_int  height ;
+	Pixmap  pixmap ;
+	GC gc;
+	GdkPixbuf * img;
+
+	img = get_root_pixmap( win);
+	if( !img)
+		return None;
+
+	if( pic_mod)
+		modify_image( img , NULL, pic_mod) ;
+	
+	if( ! x_window_get_visible_geometry( win , &x , &y , &pix_x , &pix_y , &width , &height))
+		return  None ;
+
 	pixmap = XCreatePixmap( win->display , win->my_window , ACTUAL_WIDTH(win) , ACTUAL_HEIGHT(win) ,
 			DefaultDepth( win->display , win->screen)) ;
 
