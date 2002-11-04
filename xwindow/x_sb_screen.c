128b74a1d8baecae4d3614ab6eea1466dcd83b8c
h_minami <devnull@localhost>
2002-11-04 14:40:34 +0000

    REALLY accept COMPOUND_TEXT as XDND mime type.

--- xwindow/x_sb_screen.c
+++ xwindow/x_sb_screen.c
@@ -208,6 +208,22 @@ utf8_selection_notified(
 }
 
 static void
+xct_selection_notified(
+	x_window_t *  win ,
+	u_char *  buf ,
+	size_t  len
+	)
+{
+	x_sb_screen_t *  sb_screen ;
+
+	sb_screen = (x_sb_screen_t*) win ;
+
+	/* dispatch to screen */
+	(*sb_screen->screen->window.xct_selection_notified)( &sb_screen->screen->window ,
+		buf , len) ;
+}
+
+static void
 window_deleted(
 	x_window_t *  win
 	)
@@ -677,6 +693,7 @@ x_sb_screen_new(
 	sb_screen->window.window_exposed = window_exposed ;
 	sb_screen->window.key_pressed = key_pressed ;
 	sb_screen->window.utf8_selection_notified = utf8_selection_notified ;
+	sb_screen->window.xct_selection_notified = xct_selection_notified ;
 	sb_screen->window.window_deleted = window_deleted ;
 
 	return  sb_screen ;
