2151fa5012e610596caf7f315979c67a5906e547
arakiken <devnull@localhost>
2003-08-14 14:37:56 +0000

    * Added tool/viewsfml/README,tool/viewsfml/Makefile. * viewsfml.sh: Replace charset=iso-8859-1 with charset=iso-2022-jp.

--- tool/viewsfml/viewsfml.sh
+++ tool/viewsfml/viewsfml.sh
@@ -6,4 +6,5 @@ if [ ${#} -ne 1 ]; then
 fi
 
 wget -O - "http://sourceforge.net/mailarchive/forum.php?thread_id=${1}&forum_id=3020" | \
-sed 's/\&amp;/\&/g' | sed 's/\&gt;/>/g' | sed 's/\&lt;/</g' > ~/.mlterm/${1}.html
+sed 's/\&amp;/\&/g' | sed 's/\&gt;/>/g' | sed 's/\&lt;/</g' | \
+sed 's/charset=iso-8859-1/charset=iso-2022-jp/g' > ~/.mlterm/${1}.html
