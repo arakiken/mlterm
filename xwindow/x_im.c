ad8c58ea495380a4a3bc1c722cf04b09bcfcfdea
seiichi <devnull@localhost>
2004-06-13 04:45:12 +0000

    * xwindow/x_uim.c, xwindow/x_uim.h: moved to... * inputmethod/uim/im_uim.c, inputmethod/uim/im_uim.h: this.
    Uim related things have been implemented as plug-ins(pluggable
    modules). This change enables mlterm to be compiled without -luim in
    the case of either --enable-uim or --disable-uim, and libuim is
    required from this module only.
    
    * x_im.c, x_im.h: new file, used to load input method plug-ins.
    
    * x_im_candidate_screen.c, x_im_candidate_screen.h: new file, used to
    show the candidates for conversion received from uim.
    
    * x_window.c(x_window_set_override_redirect): new function, separated
    from x_window_set_borderless_flag().
    
    * x_screen.c(draw_str, draw_str_to_eol): moved to x_draw_str.c
    
    * x_draw_str.c, x_draw_str.h: New files, separated from x_screen.c to
    share draw_str() and draw_str_to_eol() with x_im_candidate_screen.c
    and x_screen.c.
    
    * x_main_config.c, x_main_config.h: remove 'uim_engine' and add
    'input_method' option. (input_method=<input method>:<conversion engine>)
    
    * x_term_manager.c, x_term_manager.h: removed 'use_uim' option.
    
    * x_term_manager.c, x_term_manager.h (x_term_manager_add_fd,
    x_term_manager_remove_fd): new functions, used to add/remove file
    descriptors to/from the event source.

---
+++ xwindow/x_im.c
@@ -0,0 +1,130 @@
+/*
+ *	$Id$
+ */
+
+#include  <kiklib/kik_dlfcn.h>
+#include  <kiklib/kik_str.h>	/* kik_str_sep, kik_snprintf */
+
+#include  "x_im.h"
+#include  "x_term_manager.h"
+
+#ifndef  LIBDIR
+#define  IM_DIR  "/usr/local/lib/mlterm/"
+#else
+#define  IM_DIR  LIBDIR "/mlterm/"
+#endif
+
+
+typedef  x_im_t * (*x_im_new_func_t)( u_int32_t  magic ,
+				      ml_char_encoding_t  term_encoding ,
+				      x_im_event_listener_t *  im_listener ,
+				      x_im_export_syms_t *  syms ,
+				      char *  engine) ;
+
+
+/* --- static variables --- */
+
+static  x_im_export_syms_t  im_export_syms =
+{
+	ml_str_init ,
+	ml_str_delete ,
+	ml_char_combine ,
+	ml_char_set ,
+	ml_get_char_encoding ,
+	ml_parser_new ,
+	ml_conv_new ,
+	x_im_candidate_screen_new ,
+	x_term_manager_add_fd ,
+	x_term_manager_remove_fd
+
+} ;
+
+/* --- static functions --- */
+
+static x_im_new_func_t
+dlsym_im_new_func(
+	char *  im_name
+	)
+{
+	kik_dl_handle_t  handle ;
+	char * libname ;
+
+	if( ! im_name)
+	{
+		return  NULL ;
+	}
+
+	if( ! ( libname = alloca( sizeof(char) * (strlen( im_name) + 4))))
+	{
+	#ifdef  DEBUG
+		kik_debug_printf( KIK_DEBUG_TAG " malloc() failed.\n") ;
+	#endif
+
+		return  NULL ;
+	}
+
+	sprintf( libname , "im-%s" , im_name) ;
+
+	if( ! ( handle = kik_dl_open( IM_DIR , libname)))
+	{
+		return  NULL ;
+	}
+
+	return  (x_im_new_func_t) kik_dl_func_symbol( handle , "im_new") ;
+}
+
+
+/* --- global functions --- */
+
+x_im_t *
+x_im_new(
+	ml_char_encoding_t  term_encoding ,
+	x_im_event_listener_t *  im_listener ,
+	char *  input_method
+	)
+{
+	x_im_t *  im ;
+	x_im_new_func_t  func ;
+	char *  im_name ;
+	char *  im_attr ;
+
+	if( strchr( input_method , ':'))
+	{
+		im_attr = strdup( input_method) ;
+
+		if( ( im_name = kik_str_sep( &im_attr , ":")) == NULL)
+		{
+		#ifdef  DEBUG
+			kik_error_printf( "%s is illegal input method.\n" , input_method) ;
+		#endif
+
+			return  NULL ;
+		}
+	}
+	else
+	{
+		im_name = strdup( input_method) ;
+		im_attr = NULL ;
+	}
+
+	if ( ! ( func = dlsym_im_new_func( im_name)))
+	{
+		free( im_name) ;
+
+		return  NULL ;
+	}
+
+	if( ! ( im = (*func)( IM_API_COMPAT_CHECK_MAGIC ,
+			      term_encoding ,
+			      im_listener ,
+			      &im_export_syms ,
+			      im_attr)))
+	{
+		kik_error_printf( "Cound not open specified input method(%s)\n" , im_name) ;
+	}
+
+	free( im_name) ;
+
+	return  im ;
+}
+
---
+++ xwindow/x_im.h
@@ -0,0 +1,102 @@
+/*
+ *	$Id$
+ */
+
+#ifndef  __X_IM_H__
+#define  __X_IM_H__
+
+#include  <X11/Xlib.h>	/* KeySym, XKeyEvent */
+
+#include  "ml_term.h"
+#include  "x_im_candidate_screen.h"
+
+#define X_IM_PREEDIT_NOCURSOR -1
+
+/*
+ * the current preedit information
+ */
+typedef struct x_im_preedit
+{
+	ml_char_t *  chars ;
+	u_int  num_of_chars ;	/* == array size */
+	u_int  filled_len ;
+
+	int  segment_offset ;
+	int  cursor_offset ;
+
+} x_im_preedit_t ;
+
+typedef struct x_im_event_listener
+{
+	void *  self ;
+
+	int (*get_spot)( void * , ml_char_t * , int , int * , int *) ;
+	u_int (*get_line_height)( void *) ;
+	int (*is_vertical)( void *) ;
+	int (*draw_preedit_str)( void * , ml_char_t * , u_int , int) ;
+	void (*write_to_term)( void * , u_char * , size_t) ;
+
+	x_window_manager_t * (*get_win_man)( void *) ;
+	x_font_manager_t * (*get_font_man)( void *) ;
+	x_color_manager_t * (*get_color_man)( void *) ;
+
+} x_im_event_listener_t ;
+
+/*
+ * a dirty hack to replace -export-dynamic option of libtool
+ */
+typedef struct x_im_export_syms
+{
+	int (*func_ml_str_init)( ml_char_t * , u_int) ;
+	int (*func_ml_str_delete)( ml_char_t * , u_int) ;
+	int (*func_ml_char_combine)( ml_char_t * , u_char * , size_t ,
+				     mkf_charset_t , int , int , ml_color_t ,
+				     ml_color_t , int , int) ;
+	int (*func_ml_char_set)( ml_char_t * , u_char * , size_t ,
+				 mkf_charset_t  cs , int , int , ml_color_t ,
+				 ml_color_t , int , int) ;
+	int (*func_ml_get_char_encoding)( char *) ;
+	mkf_parser_t * (*func_ml_parser_new)( ml_char_encoding_t) ;
+	mkf_conv_t * (*func_ml_conv_new)( ml_char_encoding_t) ;
+	x_im_candidate_screen_t *  (*func_x_im_candidate_screen_new)(
+						x_window_manager_t * ,
+						x_font_manager_t * ,
+						x_color_manager_t * ,
+						int , u_int , int , int) ;
+	int  (*func_x_term_manager_add_fd)( int , void (*handler)(void)) ;
+	int  (*func_x_term_manager_remove_fd)( int) ;
+
+} x_im_export_syms_t ;
+
+/*
+ * the input method module object
+ */
+typedef struct x_im
+{
+	x_im_event_listener_t *  listener ;
+
+	x_im_candidate_screen_t *  cand_screen ;
+
+	x_im_preedit_t  preedit ;
+
+	/* methods */
+	void (*delete)( struct x_im *) ;
+	int (*key_event)( struct x_im * , KeySym , XKeyEvent *) ;
+	void (*focused)( struct x_im *) ;
+	void (*unfocused)( struct x_im *) ;
+	void (*draw_preedit)( struct x_im * , int) ;
+
+} x_im_t ;
+
+x_im_t *  x_im_new( ml_char_encoding_t  term_encoding ,
+		    x_im_event_listener_t *  im_listener ,
+		    char *  input_method) ;
+
+#define  IM_API_VERSION  0x0
+#define  IM_API_COMPAT_CHECK_MAGIC			\
+	(IM_API_VERSION & 0xf << 28 |			\
+	 ((sizeof( x_im_t) & 0xff) << 20) |		\
+	 ((sizeof( x_im_export_syms_t) & 0xff) << 12) |	\
+	 (sizeof( x_im_candidate_screen_t) & 0xfff))
+
+#endif
