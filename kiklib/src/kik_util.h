31c8b9187a045a5ac089e59210e6bc147c587852
h_minami <devnull@localhost>
2006-10-13 16:13:42 +0000

    * safer DIGIT_STR_LEN()

--- kiklib/src/kik_util.h
+++ kiklib/src/kik_util.h
@@ -10,19 +10,20 @@
 
 #define  K_MIN(n1,n2)  ((n1) > (n2) ? (n2) : (n1))
 
-/*
- * char  : 0 - 256 (3)
- * int16 : 0 - 65536 (5)
- * int32 : 0 - 4294967296 (10)
- * int64 : 0 - 18446744073709551616 (20)
+/* TYPE: MIN(signed) -- MAX(unsigned) (number of bytes needed)
+ * char  : -128 -- 256 (4)
+ * int16 : -32768 -- 65536 (6)
+ * int32 : -2147483648 -- 4294967296 (11)
+ * int64 : -9223372036854775808 -- 18446744073709551616 (20)
  * 
- * 40 is evenly selected in other cases just to be sure.
+ * Since log10(2^8) = 2.4..., (sizeof(n)*3) is large enough
+ * for all n >= 2.
  */
 #define  DIGIT_STR_LEN(n)  \
-	((sizeof(n) == 1) ? 3 : \
-	(sizeof(n) == 2) ? 5 : \
-	(sizeof(n) == 4) ? 10 : \
-	(sizeof(n) == 8) ? 20 : 40)
+	((sizeof(n) == 1) ? 4 : \
+	(sizeof(n) == 2) ? 6 : \
+	(sizeof(n) == 4) ? 11 : \
+	(sizeof(n) == 8) ? 20 : (sizeof(n)*3))
 
 
 #endif
