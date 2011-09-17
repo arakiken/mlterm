c69d0b0f362711ef8d6d6bc8ca0369ad7ffe3dee
arakiken <devnull@localhost>
2011-09-17 09:12:33 +0900

    xwindow/libtype/Makefile.in: @X_CFLAGS@ is added.

--- xwindow/libtype/x_font_cairo.c
+++ xwindow/libtype/x_font_cairo.c
@@ -2,6 +2,9 @@
  *	$Id$
  */
 
+#ifndef  USE_TYPE_CAIRO
 #define  USE_TYPE_CAIRO
+#endif
+
 #include  "x_font_ft.c"
 
--- xwindow/libtype/x_font_xft.c
+++ xwindow/libtype/x_font_xft.c
@@ -2,5 +2,8 @@
  *	$Id$
  */
 
+#ifndef  USE_TYPE_XFT
 #define  USE_TYPE_XFT
+#endif
+
 #include  "x_font_ft.c"
