817521664c1e38b2526e6d878dfc4ab7d9fbcffe
arakiken <devnull@localhost>
2013-10-20 20:38:31 +0900

    * doc/en/README.android: Added.

--- android/build.sh
+++ android/build.sh
@@ -5,7 +5,7 @@ ANDROID_NDK_PATH=/cygdrive/c/Users/${USER}/workspace/android-ndk-r8
 export JAVA_HOME=c:\\Program\ Files\\Java\\jdk1.7.0_02
 
 # Requires android-11 or later.
-"${ANDROID_SDK_PATH}/tools/android.bat" update project --path . --target android-14
+"${ANDROID_SDK_PATH}/tools/android.bat" update project --path . --target android-11
 (cd jni ; ${ANDROID_NDK_PATH}/ndk-build APP_ABI=all V=1)
 ant release
 
