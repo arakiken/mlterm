d8e75142f6c8d99306e9010e60fa671e0913a0e1
arakiken <devnull@localhost>
2017-12-03 01:41:03 +0900

    * 3.8.4 released.   (tag "rel-3_8_4") * configure.in, mlterm.spec, main/version.h.in,   MLTermPty.c, android/jni/version.h:   updated for 3.8.4. * PROTOCOL, man/mlterm.1: Updated.

--- java/MLTermPty.c
+++ java/MLTermPty.c
@@ -460,7 +460,7 @@ Java_mlterm_MLTermPty_nativeOpen(JNIEnv *env, jobject obj, jstring jstr_host,
   if (!str_parser) {
     bl_conf_t *conf;
 
-    bl_init_prog("mlterm", "3.8.3");
+    bl_init_prog("mlterm", "3.8.4");
     bl_set_sys_conf_dir(CONFIG_PATH);
     bl_locale_init("");
     bl_sig_child_init();
