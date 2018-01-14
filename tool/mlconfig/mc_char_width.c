e17662801b7377cc2f61ff65260434c2afcb34a3
arakiken <devnull@localhost>
2018-01-15 00:05:34 +0900

    * PROTOCOL, README.ja, mlterm.1, etc/main, etc/main.ja: Updated. * ui_screen_manager.c:   "main_config.encoding = vt_term_get_encoding(screen->term)" is moved before   create_term_intern() in open_pty(). * mc_char_width.c, mlconfig/po/*.po:   - "Set full width\nareas manually" is changed to "Add full width areas".   - "Add half width areas" button is added. * vt_parser.[ch]:   - vt_set_half_width_areas() is added.   - The char width of EF_AWIDTH property is determined in modify_ucs_property()     (called from vt_convert_to_internal_ch()) instead of put_char().   - "unicode_half_width_areas" is added to OSC 5379, 5380 and 5381. * ui_main_config.c: --halfwidth / "unicode_half_width_areas" option is added.

--- tool/mlconfig/mc_char_width.c
+++ tool/mlconfig/mc_char_width.c
@@ -19,6 +19,7 @@
 /* --- static variables --- */
 
 static char *full_width_areas;
+static char *half_width_areas;
 
 /* --- static functions --- */
 
@@ -41,6 +42,25 @@ static void edit_full_width_areas(GtkWidget *widget, gpointer data) {
   free(cur_areas);
 }
 
+static void edit_half_width_areas(GtkWidget *widget, gpointer data) {
+  char *cur_areas;
+  char *new_areas;
+
+  if (half_width_areas) {
+    cur_areas = strdup(half_width_areas);
+  } else {
+    cur_areas = mc_get_str_value("unicode_half_width_areas");
+  }
+
+  if ((new_areas = mc_get_unicode_areas(cur_areas)) &&
+      bl_compare_str(half_width_areas, new_areas) != 0) {
+    free(half_width_areas);
+    half_width_areas = new_areas;
+  }
+
+  free(cur_areas);
+}
+
 /* --- global functions --- */
 
 GtkWidget *mc_char_width_config_widget_new(void) {
@@ -49,7 +69,6 @@ GtkWidget *mc_char_width_config_widget_new(void) {
   GtkWidget *widget;
 
   vbox = gtk_vbox_new(FALSE, 0);
-  gtk_widget_show(vbox);
 
   widget = mc_flag_config_widget_new(MC_FLAG_AWIDTH);
   gtk_widget_show(widget);
@@ -60,14 +79,20 @@ GtkWidget *mc_char_width_config_widget_new(void) {
   gtk_box_pack_start(GTK_BOX(vbox), widget, FALSE, FALSE, 0);
 
   hbox = gtk_hbox_new(FALSE, 0);
-  gtk_box_pack_start(GTK_BOX(hbox), vbox, FALSE, FALSE, 0);
+  gtk_widget_show(hbox);
+  gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
 
-  widget = gtk_button_new_with_label(_("Set full width\nareas manually"));
+  widget = gtk_button_new_with_label(_("Add full width areas"));
   gtk_widget_show(widget);
   gtk_box_pack_start(GTK_BOX(hbox), widget, FALSE, FALSE, 4);
   g_signal_connect(widget, "clicked", G_CALLBACK(edit_full_width_areas), NULL);
 
-  return hbox;
+  widget = gtk_button_new_with_label(_("Add half width areas"));
+  gtk_widget_show(widget);
+  gtk_box_pack_start(GTK_BOX(hbox), widget, FALSE, FALSE, 4);
+  g_signal_connect(widget, "clicked", G_CALLBACK(edit_half_width_areas), NULL);
+
+  return vbox;
 }
 
 void mc_update_char_width(void) {
@@ -76,5 +101,9 @@ void mc_update_char_width(void) {
 
   if (full_width_areas) {
     mc_set_str_value("unicode_full_width_areas", full_width_areas);
+  }
+
+  if (half_width_areas) {
+    mc_set_str_value("unicode_half_width_areas", half_width_areas);
   }
 }
