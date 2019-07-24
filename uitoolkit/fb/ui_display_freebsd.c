3613865e2f18641e08b5a58700cc03d75df6845d
Rin Okuyama <rokuyama.rk@gmail.com>
2019-07-24 10:38:53 +0900

    Oops, forget to commit ui_display_freebsd.c in Commit #0145823.

--- uitoolkit/fb/ui_display_freebsd.c
+++ uitoolkit/fb/ui_display_freebsd.c
@@ -64,7 +64,7 @@ static int open_display(u_int depth) {
   ioctl(_display.fb_fd, FBIO_ADPINFO, &vainfo);
   ioctl(_display.fb_fd, FBIO_GETDISPSTART, &vstart);
 
-  if ((_display.fb = mmap(NULL, (_display.smem_len = vainfo.va_window_size), PROT_WRITE | PROT_READ,
+  if ((_display.fb = _display.fb_base = mmap(NULL, (_display.smem_len = vainfo.va_window_size), PROT_WRITE | PROT_READ,
                           MAP_SHARED, _display.fb_fd, (off_t)0)) == MAP_FAILED) {
     bl_error_printf("Retry another mode of resolution and depth.\n");
 
@@ -209,7 +209,7 @@ static int open_display(u_int depth) {
 error:
   if (_display.fb) {
     munmap(_display.fb, _display.smem_len);
-    _display.fb = NULL;
+    _display.fb = _display.fb_base = NULL;
   }
 
   close(_display.fb_fd);
