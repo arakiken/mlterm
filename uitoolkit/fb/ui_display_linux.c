5a114cb96511d0f1b1a446b460dd9174c720fdb9
Rin Okuyama <rokuyama.rk@gmail.com>
2019-07-24 10:13:24 +0900

    For wscons framebuffer, fix the case of fbi_fboffset != 0, which means base address of framebuffer is not page-aligned:
    - When mmap framebuffer, fbi_fboffset should be added to length of
      framebuffer. Otherwise, the highest page of framebuffer is not
      mapped, which results in memory corruption in the higher adjacent
      page, or SEGV. This is because length of mapped area is counted
      from page-truncated address.
    
    - Stop assuming that fbi_fboffset is multiple of fbi_stride. For
      one of my machine, fbi_fboffset = 2048 whereas fbi_stride = 2560.

--- uitoolkit/fb/ui_display_linux.c
+++ uitoolkit/fb/ui_display_linux.c
@@ -265,7 +265,7 @@ static int open_display(u_int depth) {
     _display.pixels_per_byte = 1;
   }
 
-  if ((_display.fb = mmap(NULL, (_display.smem_len = finfo.smem_len), PROT_WRITE | PROT_READ,
+  if ((_display.fb = _display.fb_base = mmap(NULL, (_display.smem_len = finfo.smem_len), PROT_WRITE | PROT_READ,
                           MAP_SHARED, _display.fb_fd, (off_t)0)) == MAP_FAILED) {
     goto error;
   }
@@ -349,7 +349,7 @@ static int open_display(u_int depth) {
 error:
   if (_display.fb) {
     munmap(_display.fb, _display.smem_len);
-    _display.fb = NULL;
+    _display.fb = _display.fb_base = NULL;
   }
 
   close(_display.fb_fd);
