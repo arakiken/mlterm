arakiken/mlterm

commit d810b3d0ed47906c0119202e6dda77178cee6192

* vt_parser.c:   - cs with intermediate char -> 0x100 | cs in put_char().   - parse_vt100_escape_sequence() supports "ESC $ )". * vt_drcs.c:   - vt_drcs_get_font() regards 0x100 in cs for DRCS flag.   - vt_convert_uniocde_pua_to_drcs() sets 0x100 flag to ch->cs. * vt_drcs.h: Add CS_TO_DRCS(), DRCS_TO_CS() and IS_DRCS(). * encodefilter/src/test.c: Add TEST_iso2022_parser(). * ef_iso2022_parser.c:   - Add 'int intermed' to arguments of get_charset().   - parse_escape() add an additional intermediate character (' ' for     DRCS) to cs. * ef_charset.h:   - CS_REVISION_1(), CS_REVISION_2() -> CS_REVISION_N()   - Add CS_ADD_INTERMIDIATE(), CS_DEL_INTERMEDIATE() and CS_INTERMEDIATE().   - Add IS_CSSB() and IS_CS94_STRICT().

vtemu/vt_parser.c
@@ -718,7 +718,7 @@ static void write_ttyrec_header(int fd, size_t len, int keep_time) {
     buf[2] = len;
 #endif
 
-#if __DEBUG
+#ifdef __DEBUG
     bl_debug_printf("write len %d at %d\n", len, lseek(fd, 0, SEEK_CUR));
 #endif
 
@@ -943,6 +943,7 @@ static void put_char(vt_parser_t *vt_parser, u_int32_t ch, ef_charset_t cs,
   int is_blinking;
   int is_protected;
   int is_reversed;
+  u_char intermed;
 
   if (vt_parser->w_buf.filled_len == PTY_WR_BUFFER_SIZE) {
     flush_buffer(vt_parser);
@@ -965,8 +966,14 @@ static void put_char(vt_parser_t *vt_parser, u_int32_t ch, ef_charset_t cs,
   }
 
 #ifdef __DEBUG
-  bl_debug_printf("%x %d %x => %s\n", ch, len, cs, is_fullwidth ? "Fullwidth" : "Single");
+  bl_debug_printf("ch %x cs %x => %s\n", ch, cs, is_fullwidth ? "Fullwidth" : "Single");
 #endif
+
+  if ((intermed = CS_INTERMEDIATE(cs))) {
+    if (intermed == ' ' && IS_CSSB(cs)) {
+      cs = CS_TO_DRCS(CS_DEL_INTERMEDIATE(cs));
+    }
+  }
 
   if ((prop & EF_COMBINING)
 #if !defined(NO_DYNAMIC_LOAD_CTL) || defined(USE_IND)
@@ -1113,9 +1120,7 @@ static void put_char(vt_parser_t *vt_parser, u_int32_t ch, ef_charset_t cs,
     int pic_id;
     int pic_pos;
 
-    if (cs != US_ASCII &&
-        (font = vt_drcs_get_font(vt_parser->drcs,
-                                 cs == CS_REVISION_1(US_ASCII) ? US_ASCII : cs, 0)) &&
+    if ((font = vt_drcs_get_font(vt_parser->drcs, cs, 0)) &&
         /* XXX The width of pua is always regarded as 1. (is_fullwidth is ignored) */
         vt_drcs_get_picture(font, &pic_id, &pic_pos, ch)) {
       vt_char_copy(&vt_parser->w_buf.chars[vt_parser->w_buf.filled_len],
@@ -6280,6 +6285,7 @@ inline static int parse_vt100_escape_sequence(
             } else {
               cs = CS96SB_ID(*str_p);
             }
+            cs = CS_TO_DRCS(cs);
 
             if (ps[3] <= 4 || ps[3] >= 255) {
               col_width = 15;
@@ -6604,35 +6610,57 @@ inline static int parse_vt100_escape_sequence(
                                 vt_screen_get_logical_cols(vt_parser->screen),
                                 vt_screen_get_logical_rows(vt_parser->screen));
           }
-        } else if (*(str_p - ic_num) == '(' || *(str_p - ic_num) == '$') {
+        } else if (*(str_p - ic_num) == '(' || *(str_p - ic_num) == ')' ||
+                   *(str_p - ic_num) == '$') {
           /*
-           * "ESC ("(Registered CS),
-           * "ESC ( SP"(DRCS) or "ESC $"
+           * "ESC ("(Registered CS), "ESC ( SP"(DRCS) or "ESC $"
+           * "ESC )"(Registered CS) or "ESC ) SP"(DRCS)
            * See vt_convert_to_internal_ch() about CS94MB_ID.
            */
+          int gn = 0;
 
           if (IS_ENCODING_BASED_ON_ISO2022(vt_parser->encoding)) {
             /* ESC ( will be processed in mef. */
             return 1;
           }
 
-          vt_parser->g0 = (*(str_p - ic_num) == '$') ? CS94MB_ID(*str_p) : CS94SB_ID(*str_p);
+          if (*(str_p - ic_num) == '$') {
+            if (*(str_p - ic_num + 1) == ')') {
+              vt_parser->g1 = CS94MB_ID(*str_p);
+              gn = 1;
+            }
+#if 0
+            else if (*(str_p - ic_num + 1) == '(') {
+              vt_parser->g0 = CS94MB_ID(*str_p);
+            }
+#endif
+            else {
+              vt_parser->g0 = CS94MB_ID(*str_p);
+            }
+          } else {
+            ef_charset_t cs = CS94SB_ID(*str_p);
 
-          if (!vt_parser->is_so) {
-            vt_parser->gl = vt_parser->g0;
+            if (*(str_p - ic_num + 1) == ' ') {
+              /* DRCS */
+              cs = CS_TO_DRCS(cs);
+            }
+
+            if (*(str_p - ic_num) == ')') {
+              vt_parser->g1 = cs;
+              gn = 1;
+            } else {
+              vt_parser->g0 = cs;
+            }
           }
-        } else if (*(str_p - ic_num) == ')') {
-          /* "ESC )"(Registered CS) or "ESC ( SP"(DRCS) */
 
-          if (IS_ENCODING_BASED_ON_ISO2022(vt_parser->encoding)) {
-            /* ESC ) will be processed in mef. */
-            return 1;
-          }
-
-          vt_parser->g1 = (*(str_p - ic_num) == '$') ? CS94MB_ID(*str_p) : CS94SB_ID(*str_p);
-
-          if (vt_parser->is_so) {
-            vt_parser->gl = vt_parser->g1;
+          if (gn == 0) {
+            if (!vt_parser->is_so) {
+              vt_parser->gl = vt_parser->g0;
+            }
+          } else {
+            if (vt_parser->is_so) {
+              vt_parser->gl = vt_parser->g1;
+            }
           }
         } else if (*(str_p - ic_num) == '-') {
           /* "ESC -"(Registered CS) or "ESC - SP"(DRCS) */
@@ -6643,6 +6671,11 @@ inline static int parse_vt100_escape_sequence(
           }
 
           vt_parser->g1 = CS96SB_ID(*str_p);
+
+          if (*(str_p - ic_num) == ' ') {
+            /* DRCS */
+            vt_parser->g1 = CS_TO_DRCS(vt_parser->g1);
+          }
 
           if (vt_parser->is_so) {
             vt_parser->gl = vt_parser->g1;
@@ -7679,15 +7712,6 @@ int vt_convert_to_internal_ch(vt_parser_t *vt_parser, ef_char_t *orig_ch) {
     /* See https://github.com/saitoha/drcsterm/ */
     else if ((vt_parser->unicode_policy & USE_UNICODE_DRCS) &&
              vt_convert_unicode_pua_to_drcs(&ch)) {
-      if (ch.cs == US_ASCII) {
-        vt_drcs_font_t *font;
-
-        if ((font = vt_drcs_get_font(vt_parser->drcs, US_ASCII, 0)) &&
-            vt_drcs_is_picture(font, ch.ch[0])) {
-          ch.cs = CS_REVISION_1(US_ASCII);
-        }
-      }
-
       /*
        * Go to end to skip 'if (ch.ch[0] == 0x7f) { return 0; }' in next block.
        * Otherwise, 0x10XX7f doesn't work.
@@ -7833,7 +7857,7 @@ int vt_convert_to_internal_ch(vt_parser_t *vt_parser, ef_char_t *orig_ch) {
     vt_drcs_font_t *font;
 
     if ((ch.ch[0] == 0x0 || ch.ch[0] == 0x7f) &&
-        (!(font = vt_drcs_get_font(vt_parser->drcs, US_ASCII, 0)) ||
+        (!(font = vt_drcs_get_font(vt_parser->drcs, CS_TO_DRCS(US_ASCII), 0)) ||
          !(vt_drcs_is_picture(font, ch.ch[0])))) {
       /* DECNULM is always set => discarding 0x0 */
 #ifdef DEBUG

encodefilter/src/ef_charset.h
@@ -10,9 +10,9 @@
  * DEC_SPECIAL(Ft='0').
  */
 
-/* 0x00 - 0x4e (Ft is within 0x30 and 0x7e) (0x30-0x3f is for DRCS) */
+/* 0x00 - 0x4e (Ft is within 0x30 and 0x7e) */
 #define CS94SB_ID(c) ((u_char)(c) - 0x30)
-/* 0x50 - 0x9e (Ft is within 0x30 and 0x7e) (0x30-0x3f is for DRCS) */
+/* 0x50 - 0x9e (Ft is within 0x30 and 0x7e) */
 #define CS96SB_ID(c) ((u_char)(c) + 0x20)
 /* 0xa0 - 0xbf (XXX Ft is within 0x40 and 0x5f) */
 #define CS94MB_ID(c) ((u_char)(c) + 0x60)
@@ -23,10 +23,10 @@
 /* 0xd0 - 0xdf (Ft is within 0x40 and 0x4f) */
 #define NON_ISO2022_2_ID(c) ((u_char)(c) + 0x90)
 
-/* 0x100 - 0x1bf (= 0x100 | CS9XXB_ID) */
-#define CS_REVISION_1(cs) ((cs) + 0x100)
-/* 0x200 - 0x2bf (= 0x200 | CS9XXB_ID) */
-#define CS_REVISION_2(cs) ((cs) + 0x200)
+/* 0x100 - 0x7bf (= 0xX00 | CS9XXB_ID) */
+#define CS_REVISION_N(cs, n) ((cs) + ((n) << 8))
+
+#define CS_ADD_INTERMEDIATE(cs, i) ((cs) + (((i) - 0x1f) << 11))
 
 /*
  * 'and 0xff' should be done because 0x100 - region is used for 'or cs_revision'
@@ -39,8 +39,12 @@
 
 #define IS_CS94SB(cs) ((unsigned int)((cs)&0xff) <= 0x4e) /* same as 0x00 <= .. <= 0x4e */
 #define IS_CS96SB(cs) (0x50 <= ((cs)&0xff) && ((cs)&0xff) <= 0x9e)
+#define IS_CSSB(cs) ((unsigned int)((cs)&0xff) <= 0x9e)
 #define IS_CS94MB(cs) (0xa0 <= ((cs)&0xff) && ((cs)&0xff) <= 0xbf)
 #define IS_CS96MB(cs) (0) /* always false */
+/* IS_CS94_STRICT() always returns false charsets containing intermediate chars. */
+#define IS_CS94_STRICT(cs) (((unsigned int)(cs) & 0xf8ff) <= 0x4e ||    \
+                            (0xa0 <= ((cs) & 0xf8ff) && ((cs) & 0xf8ff) <= 0x9e))
 #define IS_CS_BASED_ON_ISO2022(cs) (0x0 <= ((cs)&0xff) && ((cs)&0xff) <= 0xbf)
 /* without "(cs) != UNKNOWN_CS &&", 0xa0 <= (UNKNOWN_CS & 0xff) returns true. */
 #define IS_NON_ISO2022(cs) ((cs) != UNKNOWN_CS && 0xc0 <= ((cs)&0xff))
@@ -50,7 +54,10 @@
 #define IS_FULLWIDTH_CS(cs) (IS_CS94MB(cs) || IS_CS96MB(cs) || (0x1e0 <= (cs) && (cs) <= 0x1ff))
 #define CS_SIZE(cs) \
   ((cs) == ISO10646_UCS4_1 ? 4 : ((IS_FULLWIDTH_CS(cs) || (cs) == ISO10646_UCS2_1) ? 2 : 1))
-#define IS_ISO10646_UCS4(cs) (((cs) & ~CS_REVISION_1(0)) == ISO10646_UCS4_1)
+#define IS_ISO10646_UCS4(cs) (((cs) & ~CS_REVISION_N(0, 1)) == ISO10646_UCS4_1)
+
+#define CS_DEL_INTERMEDIATE(cs) ((cs) & 0x7ff)
+#define CS_INTERMEDIATE(cs) (((cs) & 0xf800) ? (((cs) >> 11) & 0x1f) + 0x1f : 0)
 
 /*
  * These enumeration numbers are based on iso2022 Ft(0x30-0x7e).
@@ -160,8 +167,8 @@ typedef enum ef_charset {
   /* Followings are ISO2022 based charsets with revisions. */
 
   /* Revision 1 */
-  JISX0208_1990 = CS_REVISION_1(JISX0208_1983),
-  ISO10646_UCS4_1_V = CS_REVISION_1(ISO10646_UCS4_1), /* mef original */
+  JISX0208_1990 = CS_REVISION_N(JISX0208_1983, 1),
+  ISO10646_UCS4_1_V = CS_REVISION_N(ISO10646_UCS4_1, 1), /* mef original */
 
   /* Followings are mef original classifications */
 

encodefilter/src/ef_iso2022_parser.c
@@ -51,8 +51,8 @@ inline static size_t get_cs_bytelen(ef_charset_t cs) {
 }
 
 inline static ef_charset_t get_charset(u_char ft,                 /* 0x30 - 0x7f */
-                                        int is_mb, int glyph_size, /* 94 or 96 */
-                                        int rev) {
+                                       int is_mb, int glyph_size, /* 94 or 96 */
+                                       int rev, int intermed) {
   ef_charset_t cs;
 
   if (glyph_size == 94) {
@@ -72,9 +72,9 @@ inline static ef_charset_t get_charset(u_char ft,                 /* 0x30 - 0x7f
   }
 
   if (rev == 0) {
-    return cs;
+    return CS_ADD_INTERMEDIATE(cs, intermed);
   } else if (rev == 1) {
-    return CS_REVISION_1(cs);
+    return CS_ADD_INTERMEDIATE(CS_REVISION_N(cs, 1), intermed);
   } else {
 #ifdef DEBUG
     bl_warn_printf(BL_DEBUG_TAG " unsupported charset revision.\n");
@@ -188,6 +188,7 @@ static int parse_escape(ef_iso2022_parser_t *iso2022_parser,
       } else if (IS_INTERMEDIATE(*iso2022_parser->parser.str)) {
         int is_mb;
         int rev;
+        int intermed = 0;
         u_char to_GN;
         u_char ft;
 
@@ -263,8 +264,8 @@ static int parse_escape(ef_iso2022_parser_t *iso2022_parser,
             goto reset;
           }
 
-          if (*iso2022_parser->parser.str == ' ') {
-            /* is DRCS */
+          if (IS_INTERMEDIATE(*iso2022_parser->parser.str)) {
+            intermed = *iso2022_parser->parser.str;
 
             if (ef_parser_increment(iso2022_parser) == 0) {
               /* we reach eos */
@@ -288,10 +289,10 @@ static int parse_escape(ef_iso2022_parser_t *iso2022_parser,
         }
 
         if (to_GN == CS94_TO_G0) {
-          iso2022_parser->g0 = get_charset(ft, is_mb, 94, rev);
+          iso2022_parser->g0 = get_charset(ft, is_mb, 94, rev, intermed);
         } else if (to_GN == CS94_TO_G1) {
 #ifdef DECSP_HACK
-          if (ft == '0' && !is_mb) {
+          if (ft == '0' && !is_mb && intermed == 0) {
             iso2022_parser->g1_is_decsp = 1;
           } else
 #endif
@@ -299,21 +300,21 @@ static int parse_escape(ef_iso2022_parser_t *iso2022_parser,
 #ifdef DECSP_HACK
             iso2022_parser->g1_is_decsp = 0;
 #endif
-            iso2022_parser->g1 = get_charset(ft, is_mb, 94, rev);
+            iso2022_parser->g1 = get_charset(ft, is_mb, 94, rev, intermed);
           }
         } else if (to_GN == CS94_TO_G2) {
-          iso2022_parser->g2 = get_charset(ft, is_mb, 94, rev);
+          iso2022_parser->g2 = get_charset(ft, is_mb, 94, rev, intermed);
         } else if (to_GN == CS94_TO_G3) {
-          iso2022_parser->g3 = get_charset(ft, is_mb, 94, rev);
+          iso2022_parser->g3 = get_charset(ft, is_mb, 94, rev, intermed);
         } else if (to_GN == CS96_TO_G1) {
 #ifdef DECSP_HACK
           iso2022_parser->g1_is_decsp = 0;
 #endif
-          iso2022_parser->g1 = get_charset(ft, is_mb, 96, rev);
+          iso2022_parser->g1 = get_charset(ft, is_mb, 96, rev, intermed);
         } else if (to_GN == CS96_TO_G2) {
-          iso2022_parser->g2 = get_charset(ft, is_mb, 96, rev);
+          iso2022_parser->g2 = get_charset(ft, is_mb, 96, rev, intermed);
         } else if (to_GN == CS96_TO_G3) {
-          iso2022_parser->g3 = get_charset(ft, is_mb, 96, rev);
+          iso2022_parser->g3 = get_charset(ft, is_mb, 96, rev, intermed);
         } else {
 #ifdef DEBUG
           bl_warn_printf(BL_DEBUG_TAG " illegal ISO2022 designation char %c\n", to_GN);
@@ -391,8 +392,8 @@ static int next_byte(ef_iso2022_parser_t *iso2022_parser, ef_char_t *ch) {
 
       ch->ch[ch->size++] = *iso2022_parser->parser.str;
 
-      if ((IS_CS94SB(*iso2022_parser->gl) || IS_CS94MB(*iso2022_parser->gl)) &&
-          (*iso2022_parser->parser.str == 0x20 || *iso2022_parser->parser.str == 0x7f)) {
+      if (IS_CS94_STRICT(*iso2022_parser->gl) &&
+          ((*iso2022_parser->parser.str == 0x20 || *iso2022_parser->parser.str == 0x7f))) {
         ch->cs = US_ASCII;
       } else {
         ch->cs = *iso2022_parser->gl;
@@ -408,7 +409,7 @@ static int next_byte(ef_iso2022_parser_t *iso2022_parser, ef_char_t *ch) {
         return next_byte(iso2022_parser, ch);
       }
 
-      if ((IS_CS94SB(*iso2022_parser->gr) || IS_CS94MB(*iso2022_parser->gr)) &&
+      if (IS_CS94_STRICT(*iso2022_parser->gl) &&
           (*iso2022_parser->parser.str == 0xa0 || *iso2022_parser->parser.str == 0xff)) {
 #ifdef DEBUG
         bl_warn_printf(BL_DEBUG_TAG " 0xa0/0xff appears in 94CS. skipping...\n");

encodefilter/src/test.c
@@ -1,10 +1,10 @@
 /* -*- c-basic-offset:2; tab-width:2; indent-tabs-mode:nil -*- */
 
 #ifdef BL_DEBUG
-#ifndef REMOVE_MAPPING_TABLE
-
 #include <assert.h>
 #include <pobl/bl_debug.h>
+
+#ifndef REMOVE_MAPPING_TABLE
 
 #include "ef_ucs4_jisx0208.h"
 #include "ef_ucs4_jisx0212.h"
@@ -79,9 +79,9 @@
 
 #endif
 
-/* --- global functions --- */
+/* --- static functions --- */
 
-void TEST_encodefilter(void) {
+static void TEST_mapping(void) {
 #ifdef OUTPUT_MAPPING
   FILE *fp;
   int i;
@@ -394,12 +394,44 @@ void TEST_encodefilter(void) {
   fclose(fp);
 #endif
 
-  bl_msg_printf("PASS encodefilter test.\n");
+  bl_msg_printf("PASS encodefilter mapping test.\n");
 }
 
-#else
+#endif /* REMOVE_MAPPING_TABLE */
 
-void TEST_encodefilter(void) {}
+#include <pobl/bl_str.h>
+#include <pobl/bl_mem.h>
+#include "ef_iso2022jp_parser.h"
 
+static void TEST_iso2022_parser(void) {
+  ef_parser_t *parser = ef_iso2022jp_7_parser_new();
+  const char *test[] = { "\x1b( @a", "\x1b(/@a" };
+  ef_char_t ch;
+  int count;
+
+  for (count = 0; count < sizeof(test) / sizeof(test[0]); count++) {
+    size_t len = strlen(test[count]);
+
+    (*parser->init)(parser);
+    (*parser->set_str)(parser, test[count], len);
+    assert((*parser->next_char)(parser, &ch) == 1);
+    assert(CS_INTERMEDIATE(ch.cs) == *(test[count] + len - 3));
+    assert(CS94SB_FT(ch.cs) == *(test[count] + len - 2));
+  }
+
+  (*parser->destroy)(parser);
+
+  bl_msg_printf("PASS encodefilter iso2022 parser test.\n");
+}
+
+/* --- global functions --- */
+
+void TEST_encodefilter(void) {
+#ifndef REMOVE_MAPPING_TABLE
+  TEST_mapping();
 #endif
+
+  TEST_iso2022_parser();
+}
+
 #endif /* BL_DEBUG */

vtemu/vt_drcs.c
@@ -64,10 +64,11 @@ vt_drcs_font_t *vt_drcs_get_font(vt_drcs_t *drcs, ef_charset_t cs, int create) {
     return NULL;
   }
 
-  /* CS94SB(0x30)-CS94SB(0x7e) (=0x00-0x4e), CS96SB(0x30)-CS96SB(0x7e) (0x50-0x9e) */
-  if (cs > CS96SB_ID(0x7e)) {
+  if (!IS_DRCS(cs)) {
     return NULL;
   }
+
+  cs = DRCS_TO_CS(cs);
 
   if (!drcs->fonts[cs]) {
     if (!create || !(drcs->fonts[cs] = calloc(1, sizeof(vt_drcs_font_t)))) {
@@ -177,9 +178,9 @@ int vt_convert_unicode_pua_to_drcs(ef_char_t *ch) {
 
   if (c[1] == 0x10 && 0x30 <= c[2] && c[2] <= 0x7e && c[0] == 0x00) {
     if (0x20 <= c[3] && c[3] <= 0x7f) {
-      ch->cs = CS94SB_ID(c[2]);
+      ch->cs = CS_TO_DRCS(CS94SB_ID(c[2]));
     } else if (0xa0 <= c[3] && c[3] <= 0xff) {
-      ch->cs = CS96SB_ID(c[2]);
+      ch->cs = CS_TO_DRCS(CS96SB_ID(c[2]));
     } else {
       return 0;
     }

vtemu/vt_drcs.h
@@ -6,6 +6,15 @@
 #include <pobl/bl_types.h>
 #include <mef/ef_char.h>
 
+/*
+ * XXX
+ * 0x100 == CS_REVISION_N(cs, 1) in encdoefilter, but it is assumed that
+ * single byte charsets (0x0-0x9e) doesn't have revisions.
+ */
+#define CS_TO_DRCS(cs) ((cs) | 0x100)
+#define DRCS_TO_CS(cs) ((cs) & 0xff)
+#define IS_DRCS(cs) (((cs) & 0x100) && IS_CSSB(cs))
+
 typedef struct vt_drcs_font {
   /*ef_charset_t cs;*/ /* 0x40-0x7e */
   char *glyphs[0x60];

commit b6725ab7bf20d92a0a3de0dde92541f35b9f0704

* vt_parser.c: Fix carry calculation error after 0x107e7f or 0x107eff   in define_drcs_picture().   (This bug was introduced in d810b3d0ed47906c0119202e6dda77178cee6192)   (https://github.com/arakiken/mlterm/issues/151#issuecomment-3602933164)

vtemu/vt_parser.c
@@ -2215,10 +2215,10 @@ static void define_drcs_picture(vt_parser_t *vt_parser, char *path, ef_charset_t
         offset += (96 - idx);
         idx = 0;
 
-        if (cs == CS94SB_ID(0x7e)) {
-          cs = CS96SB_ID(0x30);
-        } else if (cs == CS96SB_ID(0x7e)) {
-          cs = CS94SB_ID(0x30);
+        if (cs == CS_TO_DRCS(CS94SB_ID(0x7e))) {
+          cs = CS_TO_DRCS(CS96SB_ID(0x30));
+        } else if (cs == CS_TO_DRCS(CS96SB_ID(0x7e))) {
+          cs = CS_TO_DRCS(CS94SB_ID(0x30));
         } else {
           cs++;
         }